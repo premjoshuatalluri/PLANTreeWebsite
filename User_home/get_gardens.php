d65f309c19059f254580d78da49563acec3704b3
premjoshuatalluri <107323466+premjoshuatalluri@users.noreply.github.com>
2023-08-10 00:22:37 +0100

    gardenid pass
    passing the user id into the url

--- User_home/get_gardens.php
+++ User_home/get_gardens.php
@@ -2,7 +2,7 @@
 include 'db_connect.php';
 
 $userName = $_GET['username'];
-$sql = "SELECT garden_name FROM user_gardens WHERE username=?";
+$sql = "SELECT id, garden_name FROM user_gardens WHERE username=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $userName);
 $stmt->execute();
@@ -10,7 +10,7 @@ $result = $stmt->get_result();
 
 $gardens = [];
 while ($row = $result->fetch_assoc()) {
-    $gardens[] = $row['garden_name'];
+    $gardens[] = ['garden_name' => $row['garden_name'], 'garden_id' => $row['id']];
 }
 
 echo json_encode($gardens);
